bf3092d372f4477c730b598576b1d5b64311135f
s0shadow0BG <118588339+s0shadow0BG@users.noreply.github.com>
2022-11-25 17:17:10 +0200

    Update hack.lua

--- hack.lua
+++ hack.lua
@@ -1,5 +1,17 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Shadow hacking panel!", HidePremium = false, SaveConfig = true,IntroText = ShadowPanel, ConfigFolder = "OrionHubConfig"})
+local Window = OrionLib:MakeWindow({Name = "ShadowLibrary|AllGames", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
 
 -- TABS
 
@@ -28,7 +40,7 @@ local PTab = Window:MakeTab({
 })
 
 local CreditsTab = Window:MakeTab({
-	Name = "Credits",
+	Name = "Other",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -75,6 +87,13 @@ GamesTab:AddButton({
 	Name = "Doors,Evade",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/D4rk9690/My-scripts/main/d4rkfcker", true))()
+  	end
+})
+
+CreditsTab:AddButton({
+	Name = "Exit GUI",
+	Callback = function()
+        OrionLib:Destroy()
   	end
 })
 
